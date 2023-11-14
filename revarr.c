657df4590340c4c74c47b0df02d432601adb8155
kuldeeprajora <iamkuldeepjangid@gmail.com>
2023-11-14 23:29:42 +0530

    added a code to reverse an array

---
+++ revarr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, size);
+
+    reverseArray(arr, 0, size - 1);
+
+    printArray(arr, size);
+
+    return 0;
+}
